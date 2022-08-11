b545d8f0e14f218c59a0a8d5340b01ac2b040aa4
Mohammad Khoshbin <khoshbin.mohammad.mk@gmail.com>
2022-08-11 16:33:51 +0430

    head detection with yolov7 added

--- app.py
+++ app.py
@@ -1,50 +1,41 @@
 from flask import  Flask, render_template, Response
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, RadioField
+
+from detect import detect, load_model
+
 import cv2
 
 
-
-
 app = Flask(__name__)
+app._static_folder = 'static'
 
+# cap = cv2.VideoCapture('/home/mkh/Downloads/out.mp4')
 cap = cv2.VideoCapture(0)
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eyes_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-
-def detectAndDisplay(frame):
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame_gray = cv2.equalizeHist(frame_gray)
-
-    #-- Detect faces
-    faces = face_cascade.detectMultiScale(frame_gray)
-    for (x,y,w,h) in faces:
-        center = (x + w//2, y + h//2)
-        # frame = cv2.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        frame = cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
-
-        # faceROI = frame_gray[y:y+h,x:x+w]
-        #-- In each face, detect eyes
-        # eyes = eyes_cascade.detectMultiScale(faceROI)
-        # for (x2,y2,w2,h2) in eyes:
-        #     eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-        #     radius = int(round((w2 + h2)*0.25))
-        #     frame = cv2.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
-
-    return frame
 
 def get_frame():
+
+    frame_counter = 0
+    model = load_model('/home/mkh/Files/yolov7_my/head_re.pt')
 
     while True:
         ret, frame = cap.read()
 
+        frame_counter += 1
+        # if frame_counter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        #     frame_counter = 0
+        #     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
         if not ret:
             break
-
-        frame = detectAndDisplay(frame)
-        ret_, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        if frame_counter % 10 == 0:
+            frame = detect(frame, model)
+            ret_, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
